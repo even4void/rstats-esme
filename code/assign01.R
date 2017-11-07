754e496edd48035f726ebc70d831f2b568190647
chl <chl@aliquote.org>
2017-11-07 12:23:06 +0100

    Add assignment 1

---
+++ code/assign01.R
@@ -0,0 +1,11 @@
+## Time-stamp: <2017-11-07 12:22:57 chl>
+
+##
+## Placeholder for assignment #1.
+##
+
+
+## 1. Générer une séquence de n événements 0/1, avec p = 0,5.
+## 2. Calculer la proportion de résultats positifs obtenus en (1).
+## 3. Répéter l'expérience m = 1000 fois et vérifier la distribution des proportions observées.
+## 4. Quel est le 1er événement positif pour une expérience ? En moyenne pour m = 1000 expériences ?
