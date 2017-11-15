07bfc03f1911e5c396551d82977cd9915ff6f553
chl <chl@aliquote.org>
2017-11-15 08:25:17 +0100

    Add assignment #3

---
+++ code/assign03.R
@@ -0,0 +1,22 @@
+## Time-stamp: <2017-11-15 08:25:10 chl>
+
+##
+## Placeholder for assignment #3.
+##
+
+library(data.table)
+library(ggplot2)
+library(ggmap)
+
+## 1a. Télécharger les données Velib sur le site Open Data Paris au format CSV.
+## (http://opendata.paris.fr/explore/dataset/stations-velib-disponibilites-en-temps-reel/export/)
+## 1b. géolocaliser les 10 premières station avec `ggmap::geocode()`.
+
+
+
+## 2. Représenter la distribution des stations par arrondissemnt.
+
+
+## 3. Représenter la position géographique des stations Velib dans Paris
+## (`ggmap::get_map(location = c(2.3, 48.9), zoom = 12)`).
+
