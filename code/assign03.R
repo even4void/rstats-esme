1ef2edd3f894f462bc22d66d605acee3afe1da83
chl <chl@aliquote.org>
2017-11-15 16:07:25 +0100

    Add first solution to assignment #3

--- code/assign03.R
+++ code/assign03.R
@@ -1,4 +1,4 @@
-## Time-stamp: <2017-11-15 08:25:10 chl>
+## Time-stamp: <2017-11-15 16:06:57 chl>
 
 ##
 ## Placeholder for assignment #3.
@@ -8,15 +8,28 @@ library(data.table)
 library(ggplot2)
 library(ggmap)
 
-## 1a. Télécharger les données Velib sur le site Open Data Paris au format CSV.
+## 1. Avec les données beer_reviews.csv, afficher simultanément les évaluations
+## portant sur les critères d'arôme et d'apparence dans le même graphique.
+
+dt = fread("data/beer_reviews.csv")
+r = dt[, .(mean(review_aroma), mean(review_appearance)), by = beer_style]
+names(r) = c("style", "aroma", "appearance")
+
+r = reshape2::melt(r, measure.vars=2:3)
+
+p = ggplot(data = subset(r, value > 3.9), aes(x = reorder(style, value), y = value, color = variable))
+p = p + geom_point() + scale_color_manual("", values = c("darkgoldenrod1", "grey50"))
+p + coord_flip() + labs(x = "", y = "Rating") + theme_minimal()
+
+## 2a. Télécharger les données Velib sur le site Open Data Paris au format CSV.
 ## (http://opendata.paris.fr/explore/dataset/stations-velib-disponibilites-en-temps-reel/export/)
-## 1b. géolocaliser les 10 premières station avec `ggmap::geocode()`.
+## 2b. géolocaliser les 10 premières station avec `ggmap::geocode()`.
 
 
 
-## 2. Représenter la distribution des stations par arrondissemnt.
+## 3. Représenter la distribution des stations par arrondissemnt.
 
 
-## 3. Représenter la position géographique des stations Velib dans Paris
+## 4. Représenter la position géographique des stations Velib dans Paris
 ## (`ggmap::get_map(location = c(2.3, 48.9), zoom = 12)`).
 
