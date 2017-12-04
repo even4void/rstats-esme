da44349efb3853446f89f4db0efa41cbbc45d2dd
chl <chl@aliquote.org>
2017-12-04 13:00:44 +0100

    Add assignment #4

---
+++ code/assign04.R
@@ -0,0 +1,15 @@
+## Time-stamp: <2017-12-04 12:59:00 chl>
+
+##
+## Placeholder for assignment #4.
+##
+
+
+## 1. À partir des données MovieLense (package {recommenderlab}), trouver des films
+## visionnés par au moins 10 personnes. Quelles sont les notes attribuées à ces films ?
+
+## 2. Comment se comparent les résultats des méthodes `IBCF` et `UBCF`
+## (en termes de prédiction pour l'utilisateur 926) ?
+
+## 3. Que recommenderiez-vous à une personne ayant aimé Star Wars ?
+
