404dfa470b40c561cb34b506529f4ebc3e1868ee
chl <chl@aliquote.org>
2017-12-18 15:22:56 +0100

    Add assignment #5 and data set for TM

---
+++ code/assign05.R
@@ -0,0 +1,14 @@
+## Time-stamp: <2017-12-18 15:19:28 chl>
+
+##
+## Placeholder for assignment #5.
+##
+
+## 1. Avec les données enron, indiquer le nombre total d'employés ?
+## (Table Employee)
+
+## 2. Représenter la distribu=on annuelle des emails envoyés par les 3
+## employés les plus "ac=fs". (group_by)
+
+## 3. Parmi les 14553 échanges mail, quelle est le taux de réponse à
+## des messages ini=aux ?
